368dde697a92c65da8ffd3423b8394aa46645126
Ngos <simon.pascal.ngos@accenture.com>
2018-10-28 04:44:22 +0400

    fixed issued sonar

--- src/main/java/org/simon/pascal/Application.java
+++ src/main/java/org/simon/pascal/Application.java
@@ -1,12 +1,10 @@
 package org.simon.pascal;
 
-import java.util.UUID;
-
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
 
     public static void main(String[] args) { 
         Micronaut.run(Application.class);
-    }
+    } 
 }
